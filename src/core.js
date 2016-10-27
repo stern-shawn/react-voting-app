6be70f6f83bddd10782ef77f9b506f236174249e
Shawn Stern <stern.shawn@gmail.com>
2016-10-26 21:30:27 -0700

    Renaming tally to tallyValue in the vote function to avoid confusion with the 'tally' key. Adding tests for making sure that .next() will properly select a winner and append to the list of entries, or both if there is a tie. Also adding tests for when there should be a winner. Implementing changes to the .next() function.

--- src/core.js
+++ src/core.js
@@ -6,15 +6,23 @@ export function setEntries(state, entries) {
   return state.set('entries', List(entries))
 }
 
-// Create a new state where vote is the next two entires, and entries becomes
-// all entires after the first two
+// Create a new state where vote is the next two entries, and entries becomes
+// all entries after the first two
 export function next(state) {
-  const entries = state.get('entries')
+  // Concatenate the winner of the previous state's vote to entries
+  const entries = state.get('entries').concat(getWinners(state.get('vote')))
 
-  return state.merge({
-    vote: Map({pair: entries.take(2)}),
-    entries: entries.skip(2)
-  })
+  // If only one entry remains, clear the object and set the winner
+  if (entries.size === 1) {
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first())
+  } else {
+    return state.merge({
+      vote: Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    })
+  }
 }
 
 // If the current entry exists in vote, increment the tally by 1,
@@ -23,6 +31,22 @@ export function vote(state, entry) {
   return state.updateIn(
     ['vote', 'tally', entry],
     0,
-    tally => tally + 1
+    tallyValue => tallyValue + 1
   )
+}
+
+// Helper function, return the winning vote or both if tied
+// Use [] to make sure output is a List/array type for the receiving concat function
+function getWinners(vote) {
+  if (!vote) return []
+  const [a, b] = vote.get('pair')
+  const aVotes = vote.getIn(['tally', a], 0)
+  const bVotes = vote.getIn(['tally', b], 0)
+  if (aVotes > bVotes) {
+    return [a]
+  } else if (aVotes < bVotes) {
+    return [b]
+  } else {
+    return [a, b]
+  }
 }
--- test/core_spec.js
+++ test/core_spec.js
@@ -91,5 +91,65 @@ describe('application logic', () => {
         entries: List()
       }))
     })
+
+    it('puts the current winner back into the end of entries', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List.of('Sunshine', 'Millions', '127 Hours')
+      })
+
+      const nextState = next(state)
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Sunshine', 'Millions')
+        }),
+        entries: List.of('127 Hours', 'Trainspotting')
+      }))
+    })
+
+    it('puts both back in the case of a tie', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 3,
+            '28 Days Later': 3
+          })
+        }),
+        entries: List.of('Sunshine', 'Millions', '127 Hours')
+      })
+
+      const nextState = next(state)
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Sunshine', 'Millions')
+        }),
+        entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
+      }))
+    })
+
+    it('marks a winner when only one entry remains', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      })
+      
+      const nextState = next(state)
+      expect(nextState).to.equal(Map({
+        winner: 'Trainspotting'
+      }))
+    })
   })
 })
