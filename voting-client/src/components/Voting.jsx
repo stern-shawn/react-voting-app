442a9123c6218a5c3f147d0144daf4baf06a11e8
Shawn Stern <stern.shawn@gmail.com>
2016-10-30 19:00:27 -0700

    Adding helper functions such as isDisabled and hasVotedFor to the Voting component to disable the buttons once a vote has been cast, and to render if a vote has occured. Also adding ternary logic to render if there has been a winner, otherwise the buttons for voting.

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -6,12 +6,25 @@ export default React.createClass({
     // from undefined objects
     return this.props.pair || []
   },
+  isDisabled: function() {
+    // Bang-bang, you're a boolean! Disable the buttons if component has a
+    // truthy hasVoted prop
+    return !!this.props.hasVoted
+  },
+  hasVotedFor: function(entry) {
+    return this.props.hasVoted === entry
+  },
   render: function() {
     return <div className="voting">
-      {this.getPair().map(entry =>
+      // Conditionally render either a winner, or the buttons for voting
+      {this.props.winner ?
+        <div ref="winner">Winner is {this.props.winner}!</div> :
+        this.getPair().map(entry =>
         <button key={entry}
+                disabled={this.isDisabled()}
                 onClick={() => this.props.vote(entry)}>
           <h1>{entry}</h1>
+          {this.hasVotedFor(entry) ? <div className="label">Voted</div> : null}
         </button>
       )}
     </div>
