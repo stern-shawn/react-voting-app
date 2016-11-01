3e5f18365c7c390976bc048b35c187ac2e936b1e
Shawn Stern <stern.shawn@gmail.com>
2016-10-31 21:23:00 -0700

    Updating jsx of Results component so it can render both entries in the given pair

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -3,7 +3,15 @@ import PureRenderMixin from 'react-addons-pure-render-mixin'
 
 export default React.createClass({
   mixins: [PureRenderMixin],
+  getPair: function() {
+    return this.props.pair || []
+  },
   render: function() {
-    return <div>Hello from results!</div>
+    return <div className="results">
+      {this.getPair().map(entry =>
+      <div key={entry} className="entry">
+        <h1>{entry}</h1>
+      </div>)}
+    </div>
   }
 })
