eaf068cacfa824e06cc6c02b90f8ef0596c354d1
Shawn Stern <stern.shawn@gmail.com>
2016-11-03 22:11:08 -0700

    Updating Voting component to make use of the VOTE action and hasVoted from the server. Clicking on a button will now successfully register that the user has voted for it and will disable both.

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -3,6 +3,7 @@ import PureRenderMixin from 'react-addons-pure-render-mixin'
 import {connect} from 'react-redux'
 import Winner from './Winner'
 import Vote from './Vote'
+import * as actionCreators from '../action_creators'
 
 // Export the 'pure' Voting component
 export const Voting = React.createClass({
@@ -22,11 +23,16 @@ function mapStateToProps (state) {
     // getIn['vote', 'pair'] is the path through the state object, ie.
     // state -> vote -> pair yields ['Trainspotting', '28 Days Later']
     pair: state.getIn(['vote', 'pair']),
+    hasVoted: state.get('hasVoted'),
     winner: state.get('winner')
   }
 }
 
 // connect() connects the output of mapStateToProps into the props that
 // Voting will receive, and RETURNS a connected version of Voting component
+// In addition, it will now receive action creators such as vote() as a prop
 // Export the 'connected' version of the Voting component
-export const VotingContainer = connect(mapStateToProps)(Voting)
+export const VotingContainer = connect(
+  mapStateToProps,
+  actionCreators
+)(Voting)
