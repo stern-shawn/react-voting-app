3e1425a54c888b1f84fc7bd544daff520d14e8f1
Shawn Stern <stern.shawn@gmail.com>
2016-11-03 21:42:01 -0700

    Implementing the VOTE action to set hasVoted to the submitted entry if it is a valid entry. Tests included.

--- voting-client/src/reducer.js
+++ voting-client/src/reducer.js
@@ -6,11 +6,24 @@ function setState (state, newState) {
   return state.merge(newState)
 }
 
+// Given a state and an entry, set hasVoted to that entry if it exists in
+// the state. Return unmodified state otherwise.
+function vote (state, entry) {
+  const currentPair = state.getIn(['vote', 'pair'])
+  if (currentPair && currentPair.includes(entry)) {
+    return state.set('hasVoted', entry)
+  } else {
+    return state
+  }
+}
+
 // Use the ES6 defaults notation to have state set to a map if none given
 export default function (state = Map(), action) {
   switch (action.type) {
     case 'SET_STATE':
       return setState(state, action.state)
+    case 'VOTE':
+      return vote(state, action.entry)
   }
 
   return state
